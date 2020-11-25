38e73e04d9a1b32adb40dffafef46b5417e2e427
auserroot <fauserfzy@gmail.com>
2020-11-25 16:20:01 +0800

    main

--- JS/public.js
+++ JS/public.js
@@ -3,6 +3,15 @@ function rand(min,max){
     return parseInt(Math.random()*(max-min+1))+min;
 }
 
+// 封装一个生成随机颜色的函数
+function getColor(){
+    var color = "#"
+    for(var i=0;i<6;i++){
+        color+=rand(0,15).toString(16)
+    };
+    return color;
+}
+
 // 判断arr里面是否含有num
 function has(arr,num){
     for(var i=0;i<arr.length;i++){
