44a58f86cd0277869aa18e86d2c71fa8c2bc1363
auserroot <fauserfzy@gmail.com>
2020-12-07 11:17:46 +0800

    main

--- JS/public.js
+++ JS/public.js
@@ -6,17 +6,8 @@
 // 封装一个函数来返回指定元素的指定样式  62
 // 判断arr里面是否含有num              74
 // 随机产生一个包含n个字母或数字的字符串 84
-
-
-
-
-
-
-
-
-
-
-
+// 可以去除字符串两端的空格             102
+//封装一个animate函数，可以实现指定元素的指定属性运动到指定的目标 108
 
 */
 
@@ -97,4 +88,62 @@ function randChar(n){
         }
     }
     return str;
+}
+
+// 可以去除字符串两端的空格
+function trim(str){
+    return str.replace(/(^\s+)|(\s+$)/g,"")
+}
+
+/* 
+    封装一个animate函数，可以实现指定元素的指定属性运动到指定的目标
+        ==>dom:传入要运动的dom节点
+        ==>json:所有属性要运动到的目标的集合对象
+        ==>fn:定时完成以后执行的函数(回调函数)
+*/
+function animate(dom,json,fn){
+    clearInterval(dom.timer)
+    // 每隔20毫秒，每隔属性都缓动一小段距离
+    dom.timer = setInterval(function(){
+        // flag是一个变量，记录本次运动完了一个后，是否能停止定时器
+        var flag = true;
+        // json里面有几个键值对，就要运动几次
+        for(var attr in json){
+            
+            // 1 获取当前位置
+            if(attr=="opacity"){
+                var current = parseInt(getStyle(dom,attr)*100)
+            }else{
+                var current = parseInt(getStyle(dom,attr))
+            }
+            // 2 计算速度
+            var speed = (json[attr]-current)/10
+            speed = speed>0?Math.ceil(speed):Math.floor(speed);
+            // 3 计算下一个位置
+            var next = current+speed;
+            if(attr=="zIndex"){
+                next = json[attr];
+            }
+            // 4 有条件的定位元素
+            if(next!=json[attr]){
+                flag = false;
+            }
+            if(attr=="opacity"){
+                dom.style.opacity = next/100;
+                dom.style.filter = "alpha(opacity="+next+")";
+            }else if(attr=="zIndex"){
+                dom.style.zIndex = next;
+            }else{
+                dom.style[attr] = next+"px";
+            }
+            
+        }
+
+        if(flag==true){
+            clearInterval(dom.timer);
+            if(fn){
+                fn()
+            }
+        }
+    },20)
 }
